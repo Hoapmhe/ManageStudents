c9b498ba0d0d408c7037daa64644d05b6be64419
Phung Minh Hoa <hoaphung10112003@gmail.com>
2024-09-07 13:35:38 +0700

    model Student

---
+++ Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageStudents.Models
+{
+    public class Student : Person
+    {
+        public string StudentId { get; set; }
+        public string School { get; set; }
+        public int YearStarted { get; set; }
+        public double GPA { get; set; }
+
+        public Student() { }
+        public Student(string name, DateTime dob, string address, float height, float weight,
+                        string studentId, string school, int yearStarted, double gpa) : base(name,dob,address,height,weight)
+        {
+            StudentId = studentId;
+            School = school;
+            YearStarted = yearStarted;
+            GPA = gpa;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, Student ID: {StudentId}, School: {School}, Year Started: {YearStarted}, GPA: {GPA}";
+        }
+    }
+}

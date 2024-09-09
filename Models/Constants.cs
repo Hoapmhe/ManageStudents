b97995880b024204327d0d47cb74f475a131a428
Phung Minh Hoa <hoaphung10112003@gmail.com>
2024-09-09 19:55:00 +0700

    constants

---
+++ Models/Constants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageStudents.Models
+{
+    public static class Constants
+    {
+        public const int NAME_LENGTH_MAX = 100;
+        public const int YEAR_MIN = 1900;
+        public const int ADDRESS_LENGTH_MAX = 300;
+        public const float HEIGHT_MIN = 50.0f;
+        public const float HEIGHT_MAX = 300.0f;
+        public const float WEIGHT_MIN = 5.0f;
+        public const float WEIGHT_MAX = 1000.0f;
+        public const int STUDENT_ID_LENGTH = 10;
+        public const int SCHOOL_NAME_LENGTH_MAX = 200;
+        public const int YEAR_SCHOOL_STARTED = 1900;
+        public const float GPA_MIN = 0.0f;
+        public const float GPA_MAX = 10.0f;
+    }
+}

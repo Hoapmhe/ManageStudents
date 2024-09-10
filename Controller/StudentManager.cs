c8c0532cca4ff1af4fd2e7c7ae7745f3aa420fea
Phung Minh Hoa <hoaphung10112003@gmail.com>
2024-09-10 14:38:16 +0700

    find Student method

--- Controller/StudentManager.cs
+++ Controller/StudentManager.cs
@@ -14,7 +14,12 @@ namespace ManageStudents.Controller
         private Inputter input;
         public StudentManager()
         {
-            listStudents = new List<Student>();
+            listStudents = new List<Student>()
+            {
+                new Student("Nguyen Van A", new DateTime(2000, 5, 15), "Ha Noi", 170, 65, "HE00000001", "University A", 2018, 3.5),
+                new Student("Tran Thi B", new DateTime(2001, 9, 21), "Hai Phong", 160, 55, "HE00001111", "University B", 2019, 3.8),
+                new Student("Le Van C", new DateTime(1999, 12, 30), "Da Nang", 175, 70, "HE00000021", "University C", 2017, 3.2)
+            };
             input = new Inputter();
         }
 
@@ -46,6 +51,20 @@ namespace ManageStudents.Controller
             Student student = new Student(name, dob, address, height, weight, studentId, schoolName, yearStrarted, gpa);
             listStudents.Add(student);
             Console.WriteLine("Student added successfully!");
+        }
+
+        public void FindStudentbyId()
+        {
+            string studentId = input.GetStudentId();
+            Student student = listStudents.SingleOrDefault(s => s.StudentId == studentId);
+            if (student == null)
+            {
+                Console.WriteLine($"Student with Id {studentId} not found!");
+            }
+            else
+            {
+                Console.WriteLine(student.ToString());
+            }
         }
     }
 }
